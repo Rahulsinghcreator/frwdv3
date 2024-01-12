ab973e531b1cc38c242576373046239d33cf3891
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-12 13:59:38 +0000

    pylint: auto fixes

--- Config.py
+++ Config.py
@@ -3,7 +3,41 @@ import os
 api_id = 3748059
 api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
 chat_id = -1002137560330
-channel_id = ["@Havenmax", "@vipxshop", -1001610151919, "@flamemp", "@DragonsRealm", -1001678501895, -1001683654620,"@imaginationmp", "@furymp", -1001884446560, "@heavenmp", -1001950150014, "@milkymp", "@zeusMarketplace", "@somethingbasic", "@massmp", "@synmarket", "@crownmarketplace", "@zebmp", "@hoodmarketplace", "@Neomps", -1001899905282, -1001921053697, -1001164557116, "@ogsmp", -1001835605370, -1001885432633, "@crowmarketplace", "@nordicmp", -1001506286713, -1001966244490, "@catmps", -1002086694702]
+channel_id = [
+    "@Havenmax",
+    "@vipxshop",
+    -1001610151919,
+    "@flamemp",
+    "@DragonsRealm",
+    -1001678501895,
+    -1001683654620,
+    "@imaginationmp",
+    "@furymp",
+    -1001884446560,
+    "@heavenmp",
+    -1001950150014,
+    "@milkymp",
+    "@zeusMarketplace",
+    "@somethingbasic",
+    "@massmp",
+    "@synmarket",
+    "@crownmarketplace",
+    "@zebmp",
+    "@hoodmarketplace",
+    "@Neomps",
+    -1001899905282,
+    -1001921053697,
+    -1001164557116,
+    "@ogsmp",
+    -1001835605370,
+    -1001885432633,
+    "@crowmarketplace",
+    "@nordicmp",
+    -1001506286713,
+    -1001966244490,
+    "@catmps",
+    -1002086694702,
+]
 allow_id = "6162272468"
 max_posts = 2
 max_time = 600
