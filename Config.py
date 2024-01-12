bdd9176e83e91c772119e7f242d366234d0bfaa4
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-12 18:17:31 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,12 @@
+api_id = 3748059
+api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
+chat_id = -1002137560330
+channel_id = ["@DiveMarketPlace"]
+allow_id = "6162272468"
+max_posts = 4
+max_time = 600
+session_string = "BQCwmCUAHFk47MqjDKFvwesq4LEA4sJfyYZv-jK1-oA9eJkAsQyVzjOfbQ1vjTcaGY9ECwbSzKw3I0gkhqnJqsvPZ-dYeXdW-1YuAoBSie-hC1vabp_NG08U9sMGhRIJP6A8eFzljPHLjKC2PrQ37ZFrJUmqpJPx68_EsEoTjL1IjOzjgpjYdGC4J40J3EyrXlk0tri3CMGMWxqyHtCHmBOpPsWTEpASQI8X2FLmIrQPxXSof-huvvmmsUpdrbvnsYoexQOk_9WDads40qmv3fYvpFxjOygJMsUZGbk3py-6GXd33Y-pI4Ul7sWbC2jnR3vgAVmeRD66JWFb30Wa4RSIXXb2YQAAAAGKNpGvAA"
+REDIS_URI = None
+REDISHOST = None
+MONGO_URI = os.environ.get("MONGO_URI", None)
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
